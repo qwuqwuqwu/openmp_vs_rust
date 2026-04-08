2b3bc9e9312841c7bfc6109c90857aae882cbc9c
qwuqwuqwu <qwuqwuqwu@gmail.com>
2026-04-08 08:48:44 -0400

    mod: mod Benchmark1's script to build at first

--- Benchmark1/cpp/run_overhead.sh
+++ Benchmark1/cpp/run_overhead.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-OUT="../results_openmp.csv"
+set -e
+
+SRC="openmp_benchmark1.cpp"
 BIN="./openmp_benchmark1"
+OUT="../results_openmp.csv"
 
 REGION_REPS=100000
 BARRIER_REPS=100000
@@ -9,6 +12,15 @@ ATOMIC_REPS=100000
 TRIALS=5
 
 THREADS_LIST=(1 2 4 8 16 32)
+
+# Build
+echo "Building $SRC..."
+g++ -O3 -fopenmp -std=c++17 -o "$BIN" "$SRC"
+echo "Done. Binary: $BIN"
+echo ""
+
+# Run
+echo "Running: region_reps=$REGION_REPS, barrier_reps=$BARRIER_REPS, atomic_reps=$ATOMIC_REPS, trials=$TRIALS..."
 
 $BIN "${THREADS_LIST[0]}" $REGION_REPS $BARRIER_REPS $ATOMIC_REPS $TRIALS --csv --warmup > "$OUT"
 
--- Benchmark1/rust/run_overhead.sh
+++ Benchmark1/rust/run_overhead.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-OUT="../results_rust.csv"
+set -e
+
 BIN="./target/release/rust_benchmark1"
+OUT="../results_rust.csv"
 
 REGION_REPS=100000
 BARRIER_REPS=100000
@@ -9,6 +11,15 @@ ATOMIC_REPS=100000
 TRIALS=5
 
 THREADS_LIST=(1 2 4 8 16 32)
+
+# Build
+echo "Building..."
+cargo build --release 2>&1
+echo "Done. Binary: $BIN"
+echo ""
+
+# Run
+echo "Running: region_reps=$REGION_REPS, barrier_reps=$BARRIER_REPS, atomic_reps=$ATOMIC_REPS, trials=$TRIALS..."
 
 $BIN "${THREADS_LIST[0]}" $REGION_REPS $BARRIER_REPS $ATOMIC_REPS $TRIALS --csv --warmup > "$OUT"
 
